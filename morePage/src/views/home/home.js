85118b8e2bf4be3637ffe161d54a4f84b0c65d6c
shangwei.chen <laryers@163.com>
2018-09-10 01:47:42 +0800

    1

--- morePage/src/views/home/home.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './home'
-
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-})
--- morePage/src/views/login/login.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './login'
-
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app'
-})
