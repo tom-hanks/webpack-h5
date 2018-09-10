21a82c50a85579251240b2fcff6d4ace86c4ed9d
shangwei.chen <laryers@163.com>
2018-09-10 10:38:14 +0800

    自己修改的

---
+++ morePageziji/morePage/src/custom/customPack.js
@@ -0,0 +1,3 @@
+module.exports = {
+  arrycustomPack: ['app']
+}
---
+++ morePageziji/morePage/src/views/login/login.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './login'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app'
+})
